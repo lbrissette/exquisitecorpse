c44dcd4a4345663568c34734a549fcae7d461e1d
lbrissette <brissette.l@northeastern.edu>
2024-02-24 21:38:41 -0500

    no drawing outside screen

--- js/drawinggame.js
+++ js/drawinggame.js
@@ -6,8 +6,8 @@ const ctx = canvas.getContext('2d');
 const canvasOffsetX = canvas.offsetLeft;
 const canvasOffsetY = canvas.offsetTop;
  
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
+canvas.width = window.innerWidth * .75;
+canvas.height = window.innerHeight * .75;
 // a comment!
 let isPainting = false;
 let lineWidth = 5;
